f96aef1f5e7e0f8fd73f322d755990c554227b50
jk090311 <kirbyragasajo09@gmail.com>
2025-03-25 23:57:56 +0800

    subject table

--- Database/adviser_list.sql
+++ Database/adviser_list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 22, 2025 at 06:30 PM
+-- Generation Time: Mar 25, 2025 at 04:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,9 +40,34 @@ CREATE TABLE `advisers` (
 --
 
 INSERT INTO `advisers` (`ID`, `adviserFullName`, `adviserContactNumber`, `adviserGrLvl`, `adviserSection`) VALUES
-(2, 'Lorraine Torres', '09751202372', '8', 'Bronze'),
 (3, 'Kian Daenielle Garcia', '09761202372', '10', 'Bronze'),
-(7, 'Jesusa Saavedra', '09451202372', '9', 'Crank');
+(7, 'Jesusa Saavedra', '09451202372', '9', 'Crank'),
+(11, 'Maria Josefa Cruz', '09761202372', '7', 'Bronze'),
+(12, 'Marvin Cyrill R. Palomar', '0963258465', '10', 'Aqua');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subject_name` varchar(255) NOT NULL,
+  `subject_picture` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_name`, `subject_picture`, `created_at`) VALUES
+(17, 'AP', 'ap.jpg', '2025-03-25 03:14:34'),
+(18, 'Science', 'science.jpg', '2025-03-25 03:15:19'),
+(19, 'Filipino', 'filipino.jpg', '2025-03-25 03:18:29'),
+(21, 'science biology', 'calendar_747310.png', '2025-03-25 06:04:31'),
+(22, 'grgdrg', '241463411_379540937093962_5358002750396125332_n.png', '2025-03-25 06:06:15');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +80,12 @@ ALTER TABLE `advisers`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +93,13 @@ ALTER TABLE `advisers`
 -- AUTO_INCREMENT for table `advisers`
 --
 ALTER TABLE `advisers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
